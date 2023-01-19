985295792316eab637fd219a63fc22620c01342a
Giulia <giuliac35@gmail.com>
2023-01-19 14:14:18 +0100

    Call the two WordGames methods from the main method

--- src/it/develhope/javadoc1/Start.java
+++ src/it/develhope/javadoc1/Start.java
@@ -3,6 +3,7 @@ package it.develhope.javadoc1;
 public class Start {
     public static void main(String[] args) {
         WordGames wordGames = new WordGames();
-
+        wordGames.addHelloWord("word");
+        wordGames.getFullName("Mario", "Rossi");
     }
 }
