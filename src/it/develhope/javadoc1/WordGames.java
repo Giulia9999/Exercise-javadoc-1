883f9287be1274d3ec71dd7de5d3b453986b77e3
Giulia <giuliac35@gmail.com>
2023-01-19 14:30:54 +0100

    Add javadoc comments on the WordGame methods

--- src/it/develhope/javadoc1/WordGames.java
+++ src/it/develhope/javadoc1/WordGames.java
@@ -5,10 +5,21 @@ package it.develhope.javadoc1;
  * @author Giulia
  */
 public class WordGames {
+    /**
+     * The goal of the method is to achieve the phrase "Hello word"
+     * @param word It's a String type parameter and should represent the final word of "Hello word"
+     * @return 2 strings
+     */
     public String addHelloWord(String word){
         return "Hello" + word;
     }
 
+    /**
+     * This method returns the name and surname of someone
+     * @param name A string type name of a person
+     * @param surname A string type surname of a person
+     * @return The concatenation of name and surname
+     */
     public String getFullName(String name, String surname){
         return name + surname;
     }
