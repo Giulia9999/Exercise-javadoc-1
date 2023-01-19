e24938bc2193c9e2e4dcf382fe2596d8be95f471
Giulia <giuliac35@gmail.com>
2023-01-19 14:16:15 +0100

    Print methods results

--- src/it/develhope/javadoc1/Start.java
+++ src/it/develhope/javadoc1/Start.java
@@ -3,7 +3,10 @@ package it.develhope.javadoc1;
 public class Start {
     public static void main(String[] args) {
         WordGames wordGames = new WordGames();
-        wordGames.addHelloWord("word");
-        wordGames.getFullName("Mario", "Rossi");
+        String helloWord = wordGames.addHelloWord(" word");
+        String completeName = wordGames.getFullName("Mario", "Rossi");
+
+        System.out.println(helloWord);
+        System.out.println(completeName);
     }
 }
